b38362f9cfb3922b7b511d8b1c3aee3461bdb955
Jader-Moura-Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2023-10-28 18:45:23 -0300

    Add files via upload
    Objeto Tablet, ainda em desenvolvimento

---
+++ tablet.rb
@@ -0,0 +1,39 @@
+class Tablet
+	attr_reader :marca, :modelo, :tamanho_tela, :armazenamento_interno, :cor, :ram, :sistema_operacional
+
+	def initialize (marca, modelo, tamanho_tela, armazenamento_interno, cor, ram, sistema_operacional)
+		@marca = marca
+		@modelo = modelo
+		@tamanho_tela = tamanho_tela
+		@armazenamento_interno = armazenamento_interno
+		@cor = cor
+		@ram = armazenamento_interno
+		@sistema_operacional = sistema_operacional
+		@preco = preco
+		@ligado = false
+	end
+
+	def ligar
+		ligado = true
+		puts "O tablet foi ligado."
+	end
+
+	def desligar
+		@ligado = false
+		puts "O tablet foi desligado."
+	end
+
+	def instalar_aplicativo(aplicativo)
+		puts "Instalando o aplicativo #{aplicativo}..."
+	end
+
+	def tirar_foto
+		puts "Tirando uma foto..."		
+	end
+
+	def obter_informacoes
+				"Marca: #{@marca}, Modelo: #{@modelo}, Tamanho de tela: #{@tamanho_tela}, Capacidade de armazenamento: #{@armazenamento_interno}, cor: #{@cor}, Memória RAM: #{@ram}, Sistema Operacional: #{@sistema_operacional}"
+	end
+end
+
+
