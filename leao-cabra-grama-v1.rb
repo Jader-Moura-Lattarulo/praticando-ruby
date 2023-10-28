805f077a55f4b3c70dff4d2de4ceb88b1f206ca0
Jader-Moura-Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2023-10-28 19:54:10 -0300

    Add files via upload

--- leao-cabra-grama-v1.rb
+++ leao-cabra-grama-v1.rb
@@ -1,4 +1,4 @@
-puts "O objetivo desse desafio é levar um leão, uma cabra e um pacote de grama de um lado para o outro de um rio."
+puts "O objetivo desse desafio é levar em um barco um leão, uma cabra e um pacote de grama de um lado para o outro de um rio."
 puts "Regra 01: O leão nunca pode ficar sozinho com a cabra e nem a cabra sozinha com a grama"
 puts "Regra 02: Você só pode levar um ou nenhum deles por vez."
 puts "Escolha uma opção:"
@@ -6,6 +6,93 @@ puts "\tOpção 01: Levar apenas o leão."
 puts "\tOpção 02: Levar apenas o pacote de grama."
 puts "\tOpção 03: Levar apenas a cabra."
 puts "Digite apenas o número da opção desejada:"
+
+class Barco
+    attr_reader :capacidade_max
+
+    def initialize (capacidade_max)
+        @capacidade_max = 1
+    end
+
+    def mover_margem_1
+
+    end
+
+    def mover_margem_2
+        
+    end
+
+    def carregar
+        
+    end
+
+    def descarregar
+        
+    end
+end
+
+class Leao
+    attr_reader :comer, :ficar_ok
+
+    def initialize (comer, ficar_ok)
+        @comer = comer
+        ficar_ok = ficar_ok
+    end
+
+    def escolhido
+        
+    end
+
+    def colocado_no_barco
+        
+    end
+
+    def retirado_do_barco
+        
+    end
+end
+
+class Cabra
+    attr_reader :comer, :ficar_ok
+
+    def initialize (comer, ficar_ok)
+        @comer = comer
+        ficar_ok = ficar_ok
+    end
+
+    def escolhido
+        
+    end
+
+    def colocado_no_barco
+        
+    end
+
+    def retirado_do_barco
+        
+    end
+end
+
+class Pacote_Grama
+    attr_reader :comer, :ficar_ok
+
+    def initialize (ficar_ok)
+        ficar_ok = ficar_ok
+    end
+
+    def escolhido
+        
+    end
+
+    def colado_no_barco
+        
+    end
+
+    def retirado_do_barco
+        
+    end
+end
+=begin
 escolha = gets.chomp.to_i
 while escolha <= 0 || escolha >= 4
     puts "Opção invalida, por favor escolha uma das opções abaixo:"
@@ -41,3 +128,4 @@ while escolha <= 0 || escolha >= 4
     puts "Digite apenas o número da opção desejada:"
     escolha = gets.chomp.to_i
 end    
+=end
