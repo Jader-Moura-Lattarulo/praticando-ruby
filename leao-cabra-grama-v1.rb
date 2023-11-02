ebc45a6c3d670b32087810bd74a814f6889fb43f
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2023-11-02 11:35:51 -0300

    Add files via upload
    Adicionei funcionalidade de seleção de itens e realizei alteração nas classes.

--- leao-cabra-grama-v1.rb
+++ leao-cabra-grama-v1.rb
@@ -1,131 +1,82 @@
-puts "O objetivo desse desafio é levar em um barco um leão, uma cabra e um pacote de grama de um lado para o outro de um rio."
-puts "Regra 01: O leão nunca pode ficar sozinho com a cabra e nem a cabra sozinha com a grama"
-puts "Regra 02: Você só pode levar um ou nenhum deles por vez."
-puts "Escolha uma opção:"
-puts "\tOpção 01: Levar apenas o leão."
-puts "\tOpção 02: Levar apenas o pacote de grama."
-puts "\tOpção 03: Levar apenas a cabra."
-puts "Digite apenas o número da opção desejada:"
+module Escolhido
+    def escolha2
+        escolha2 = 0
+        puts "Você selecionou: #{nome}"
+        puts "O que vai fazer agora:"
+        puts "\tOpção 01: Levar para a outra margem."
+        puts "\tOpção 02: Trocar a escolha."
+        puts "Digite apenas o número da opção desejada:"
+        escolha2 = gets.chomp.to_i
+        while escolha2 <= 0 || escolha2 >= 3
+            puts "Opção invalida, por favor escolha uma das opções abaixo:"
+            puts "\tOpção 01: Levar para a outra margem."
+            puts "\tOpção 02: Trocar a escolha."
+            puts "Digite apenas o número da opção desejada:"
+            escolha2 = gets.chomp.to_i
+        end
+    end
+end
 
 class Barco
-    attr_reader :capacidade_max
 
-    def initialize (capacidade_max)
-        @capacidade_max = 1
-    end
+    def initialize
+        @nome = "Barco"
+        escolha = 0
+        puts "Você está na margem 01, na sua frente tem um leão, uma cabra e um saco de grama."
+        puts "Qual item você escolhe?"
 
-    def mover_margem_1
+        escolha = gets.chomp.to_i
 
-    end
+        if escolha == 1 
+                leao = Leao.new
+                leao.escolha2
+            elsif escolha == 2
+                pacote_grama = PacoteGrama.new
+                pacote_grama.escolha2
+            elsif escolha == 3
+                cabra = Cabra.new
+                cabra.escolha2
+        end 
 
-    def mover_margem_2
-        
-    end
-
-    def carregar
-        
-    end
-
-    def descarregar
-        
+        while escolha <= 0 || escolha >= 4
+            puts "Opção invalida, por favor escolha uma das opções abaixo:"
+            puts "\tOpção 01: Levar apenas o leão."
+            puts "\tOpção 02: Levar apenas o pacote de grama."
+            puts "\tOpção 03: Levar apenas a cabra."
+            puts "Digite apenas o número da opção desejada:"
+            escolha = gets.chomp.to_i
+            if escolha == 1 
+                leao = Leao.new
+                leao.escolha2
+            elsif escolha == 2
+                pacote_grama = PacoteGrama.new
+                pacote_grama.escolha2
+            elsif escolha == 3
+                cabra = Cabra.new
+                cabra.escolha2
+            end     
+        end 
     end
 end
 
 class Leao
-    attr_reader :comer, :ficar_ok
-
-    def initialize (comer, ficar_ok)
-        @comer = comer
-        ficar_ok = ficar_ok
+    include Escolhido
+    def initialize
+        @nome = "Leão"
     end
+end
 
-    def escolhido
-        
-    end
-
-    def colocado_no_barco
-        
-    end
-
-    def retirado_do_barco
-        
+class PacoteGrama
+    include Escolhido
+    def initialize
+        @nome = "Pacote de grama"       
     end
 end
 
 class Cabra
-    attr_reader :comer, :ficar_ok
-
-    def initialize (comer, ficar_ok)
-        @comer = comer
-        ficar_ok = ficar_ok
-    end
-
-    def escolhido
-        
-    end
-
-    def colocado_no_barco
-        
-    end
-
-    def retirado_do_barco
-        
+    include Escolhido
+    def initialize
+        @nome = "Cabra"
     end
 end
-
-class Pacote_Grama
-    attr_reader :comer, :ficar_ok
-
-    def initialize (ficar_ok)
-        ficar_ok = ficar_ok
-    end
-
-    def escolhido
-        
-    end
-
-    def colado_no_barco
-        
-    end
-
-    def retirado_do_barco
-        
-    end
-end
-=begin
-escolha = gets.chomp.to_i
-while escolha <= 0 || escolha >= 4
-    puts "Opção invalida, por favor escolha uma das opções abaixo:"
-    puts "\tOpção 01: Levar apenas o leão."
-    puts "\tOpção 02: Levar apenas o pacote de grama."
-    puts "\tOpção 03: Levar apenas a cabra."
-    puts "Digite apenas o número da opção desejada:"
-    escolha = gets.chomp.to_i
-end    
     
-case escolha
-when 1
-    puts "A cabra ficou sozinha com o pacote de grama."
-    puts "Você perdeu, reinicie o desafio."
-when 2
-    puts "A cabra ficou sozinha com o leão."
-    puts "Você perdeu, reinicie o desafio."
-when 3
-    puts "Boa escolha!"
-    puts "A cabra ficou do outro lado e você voltou."
-    puts "Quem você pega agora? Escolha uma opção:"
-    puts "\tOpção 01: Leão."
-    puts "\tOpção 02: Pacote de grama."
-    puts "Digite apenas o número da opção desejada:"
-    escolha = gets.chomp.to_i
-end
-
-while escolha <= 0 || escolha >= 4
-    puts "Opção invalida, por favor escolha uma das opções abaixo:"
-    puts "\tOpção 01: Levar o leão."
-    puts "\tOpção 02: Levar o pacote de grama."
-    puts "\tOpção 03: Levar a cabra."
-    puts "Digite apenas o número da opção desejada:"
-    escolha = gets.chomp.to_i
-end    
-=end
